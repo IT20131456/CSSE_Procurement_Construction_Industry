35534560e0f4c47d547d31781617494a12bb3426
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-10-26 13:42:19 +0530

    Hello World

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    SafeAreaView,
+    ScrollView,
+    ImageBackground,
+    TextInput
+  } from 'react-native';
+
+export default function index() {
+  return (
+    <SafeAreaView>
+        <Text>Hello World Skyline</Text>
+    </SafeAreaView>
+  )
+}
