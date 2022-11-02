86a66fb8ab5605c5c6b72be664e5d4adabc9eb39
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-11-03 00:15:01 +0530

    Update Comments

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -52,21 +52,12 @@ export default function Login() {
         returnKeyType="next"
         value={email.value}
         onChangeText={text => setEmail({value: text, error: ''})}
-        error={!!email.error}
-        errorText={email.error}
-        autoCapitalize="none"
-        autoCompleteType="email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
       />
 
       <TextInput
         label="NIC No"
-        returnKeyType="done"
         value={password.value}
         onChangeText={text => setPassword({value: text, error: ''})}
-        error={!!password.error}
-        errorText={password.error}
         secureTextEntry
       />
 
