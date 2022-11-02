cc8c11127441aa9c9c7f102fee31aca01331bb72
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-11-02 11:24:12 +0530

    Update Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function Home() {
 
