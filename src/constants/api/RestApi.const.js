170c4c7efe738f0ceb20181aed89642f739f698e
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-11-11 19:48:31 +0530

    Add Constant directory and api constants

---
+++ src/constants/api/RestApi.const.js
@@ -0,0 +1,17 @@
+//UserApi
+export const USER_LOGIN_PATH = '/user/login';
+export const USER_REGISTER_PATH = '/user/registration';
+
+//OrderApi
+export const ORDER_ADD_PATH = '/tender/add';
+export const ORDER_GET_ALL_PATH = '/tender/getall';
+export const ORDER_GET_BY_ID_PATH = '/tender/get/';
+export const ORDER_UPDATE_PATH = '/tender/update/';
+export const ORDER_DELETE_PATH = '/tender/delete/';
+
+//RecivedOrderApi
+export const RECIVED_ORDER_ADD_PATH = '/receivedOrder/add';
+export const RECIVED_ORDER_GET_ALL_PATH = '/receivedOrder/getAll';
+export const RECIVED_ORDER_GET_BY_ID_PATH = '/receivedOrder/get/';
+export const RECIVED_ORDER_UPDATE_PATH = '/receivedOrder/update/';
+export const RECIVED_ORDER_DELETE_PATH = '/receivedOrder/delete/';
